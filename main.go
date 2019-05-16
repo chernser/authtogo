003018e8aa5f82ec8afa8ad9949bff9e9e8f95ef
Sergey Chernov <chernser@gmail.com>
2019-05-16 12:34:18 +0300

    Added http router. Added saml library

--- main.go
+++ main.go
@@ -1,41 +1,49 @@
 package main
 
 import (	
-	"net/http"
+	// "net/http"
 
 	"github.com/valyala/fasthttp"
-	_ "github.com/rs/zerolog"
+	"github.com/buaazp/fasthttprouter"
+	// "github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	
 	"./oauth2"
+	"./saml"
 )
 
 type AuthServer struct {
 	OAuth2Handler *oauth2.OAuth2Server
+	OAuth2AuthorizePath string
+	OAuth2TokenPath string
+	SamlSPServer *saml.SamlSPServer
 }
 
-func (srv *AuthServer) HandleRequest(ctx *fasthttp.RequestCtx) {
+func (srv *AuthServer) handleOAuthAuthorize(ctx *fasthttp.RequestCtx) {
 	log.Debug().Msgf("Handling request: %s", ctx.Path())
-	switch (string(ctx.Path())) {
-	case "/auth/oauth2/authorize":
-		fallthrough
-	case "/auth/oauth2/token":
-		srv.OAuth2Handler.RequestHandler(ctx)
-		break
-	default: 
-		log.Error().Msg("Unknown auth request")
-		ctx.Error("Unknown authentication mechanis", http.StatusNotFound)
-	}
-	log.Debug().Msg("Request handled")
+	srv.OAuth2Handler.RequestHandler(ctx)
+}
+
+func (srv *AuthServer) handleOAuthTokenRequest(ctx *fasthttp.RequestCtx) {
+	log.Debug().Msgf("Handling request: %s", ctx.Path())
+	srv.OAuth2Handler.RequestHandler(ctx)
 }
 
 func main() {
 	log.Info().Msg("Starting Auth Service")
 	
 	initConfiguration()
-	srvCtx := &AuthServer{
+	srvCtx := &AuthServer{		
 		OAuth2Handler: oauth2.InitOAuth2Server(),
+		OAuth2AuthorizePath: "/auth/oauth2/authorize",
+		OAuth2TokenPath: "/auth/oauth2/token",
+		SamlSPServer: saml.InitSamlSPServer(nil),
 
 	}
-	fasthttp.ListenAndServe(":8088", srvCtx.HandleRequest)
+
+	router := fasthttprouter.New()
+	router.POST(srvCtx.OAuth2AuthorizePath, srvCtx.handleOAuthAuthorize)
+	router.POST(srvCtx.OAuth2TokenPath, srvCtx.handleOAuthTokenRequest)
+
+	fasthttp.ListenAndServe(":8088", router.Handler)
 }
--- saml/saml.go
+++ saml/saml.go
@@ -1,1 +1,35 @@
 package saml
+
+import (
+	"github.com/rs/zerolog/log"
+)
+
+type SamlSPServerConfig struct {
+
+}
+
+type SamlSPServer struct { 
+
+}
+
+
+type SamlIDPServerConfig struct {
+
+}
+
+type SamlIDPServer struct {
+
+}
+
+
+
+func InitSamlSPServer(config *SamlSPServerConfig) (*SamlSPServer) {
+	log.Info().Msg("Init SAML SP Server")
+	return nil
+}
+
+
+func InitSamlIDPServer(config *SamlIDPServerConfig) (*SamlIDPServer) {
+	log.Info().Msg("Init SAML IDP Server")
+	return nil
+}
